8050b81d09a12de1c118af30c6f2247107544996
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2026-05-22 18:56:10 +0530

    pre laod added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, useAuth, UserButton, useUser } from '@clerk/react'
 import './App.css'
 
 function App() {
-
+ const { isLoaded, isSignedIn } = useAuth()
+  const { user } = useUser()
+  
+  console.log('Clerk loaded:', isLoaded)
+  console.log('User signed in:', isSignedIn)
+  console.log('User:', user)
   return (
     <>
      <header>
+         <pre>
+        Loaded: {String(isLoaded)}<br />
+        Signed In: {String(isSignedIn)}<br />
+        User ID: {user?.id || 'None'}
+      </pre>
         <Show when="signed-out">
           <SignInButton mode="modal" />
           <SignUpButton mode='modal' />
